eb49a8e75eb515a758c7595c0941d09c606b5d64
Josue Montano <josuemontanoa@gmail.com>
2020-12-04 08:45:52 -0400

    Create the fastapi app

---
+++ src/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import FileResponse, HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+from .models import SUBSCRIPTIONS, Subscription
+from .push import send_notification
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="src/static"), name="static")
+
+
+
+@app.get("/", response_class=HTMLResponse)
+async def home_page(request: Request):
+    templates = Jinja2Templates(directory="src/templates")
+    return templates.TemplateResponse("home.jinja2", {"request": request})
